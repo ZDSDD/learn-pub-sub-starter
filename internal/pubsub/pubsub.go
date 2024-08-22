8cbc66ffcf325cbf306a06f03ec38b2a8dca28c3
zdsdd <sebasadowy@gmail.com>
2024-08-22 20:47:43 +0200

    add pubsub function to publish json

--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -1,1 +1,22 @@
 package pubsub
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	ampq "github.com/rabbitmq/amqp091-go"
+)
+
+func PublishJSON[T any](ch *ampq.Channel, exchange, key string, val T) error {
+	bytes, err := json.Marshal(val)
+
+	// Fatal error (prints and exits)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return err
+	}
+
+	ch.PublishWithContext(context.Background(), exchange, key, false, false, ampq.Publishing{ContentType: "application/json", Body: bytes})
+	return nil
+}
