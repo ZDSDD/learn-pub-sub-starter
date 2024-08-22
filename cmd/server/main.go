f628f759a8296c7a3199b8e90aa9d7101b3d29e8
zdsdd <sebasadowy@gmail.com>
2024-08-22 20:54:49 +0200

    publish playing state to the channel

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -13,13 +15,20 @@ func main() {
 	var connectionString string = "amqp://guest:guest@localhost:5672/"
 
 	connection, err := amqp.Dial(connectionString)
-
 	if err != nil {
 		fmt.Printf("There was an error creating connection")
 		return
 	}
 	fmt.Printf("Connected successfuly!")
+
 	defer connection.Close()
+
+	channel, err := connection.Channel()
+	if err != nil {
+		fmt.Printf("\nthere was en arror creating connection channel %v\n", err)
+		return
+	}
+	pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: false})
 
 	// wait for ctrl+c
 	signalChan := make(chan os.Signal, 1)
