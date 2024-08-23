34a2bd84c4d8b8eec061a7af145d9231f261bb45
zdsdd <sebasadowy@gmail.com>
2024-08-23 15:22:34 +0200

    add messages processing

--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -43,3 +43,54 @@ func DeclareAndBind(
 
 	return channel, queue, err
 }
+
+type MessageProcessor[T any] struct {
+	handler func(T)
+}
+
+func NewMessageProcessor[T any](handler func(T)) *MessageProcessor[T] {
+	return &MessageProcessor[T]{handler: handler}
+}
+
+func (mp *MessageProcessor[T]) ProcessMessage(msg amqp.Delivery) {
+	fmt.Printf("Received a message: %s\n", msg.Body)
+	fmt.Println("Unmarshalling...")
+	var msgUnmarshaled T
+	if err := json.Unmarshal(msg.Body, &msgUnmarshaled); err != nil {
+		fmt.Printf("Error unmarshalling message: %v\n", err)
+		// Handle error (maybe reject the message or log it)
+		return
+	}
+	mp.handler(msgUnmarshaled)
+	msg.Ack(false)
+}
+
+func (mp *MessageProcessor[T]) ProcessDeliveries(deliveries <-chan amqp.Delivery) {
+	go func() {
+		for msg := range deliveries {
+			mp.ProcessMessage(msg)
+		}
+	}()
+}
+
+func SubscribeJSON[T any](
+	conn *amqp.Connection,
+	exchange, queueName, key string,
+	simpleQueueType SimpleQueueType,
+	handler func(T),
+) error {
+	channel, queue, err := DeclareAndBind(conn, exchange, queueName, key, simpleQueueType)
+	if err != nil {
+		return err
+	}
+
+	deliveryChan, err := channel.Consume(queue.Name, "", false, false, false, false, nil)
+	if err != nil {
+		return err
+	}
+
+	processor := NewMessageProcessor(handler)
+	processor.ProcessDeliveries(deliveryChan)
+
+	return nil
+}
